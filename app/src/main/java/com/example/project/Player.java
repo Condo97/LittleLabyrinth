0c1b814ec51d5b67704f3ff7ffab95ad649a51c6
atwde <robertsaustin34@yahoo.com>
2020-04-19 20:27:12 -0400

    Removed animations via comments, plan to re add later

--- app/src/main/java/com/example/project/Player.java
+++ app/src/main/java/com/example/project/Player.java
@@ -12,60 +12,60 @@ import android.graphics.Rect;
 public class Player implements GameObject {
     private Rect rect;
     private int color;
-    private Animation idle;
-    private Animation moveLeft;
-    private Animation moveRight;
+    //private Animation idle;
+    //private Animation moveLeft;
+    //private Animation moveRight;
     private AnimationManager animationManager;
 
     public Player(Rect rect, int color){
         this.rect = rect;
         this.color = color;
 
-        BitmapFactory bitmapFactory = new BitmapFactory();
-        Bitmap idleImg = bitmapFactory.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.ball2);
-        Bitmap rotate30R = bitmapFactory.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.ball2);
-        Bitmap rotate60R = bitmapFactory.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.ball2);
+        //BitmapFactory bitmapFactory = new BitmapFactory();
+        //Bitmap idleImg = bitmapFactory.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.ball2);
+        //Bitmap rotate30R = bitmapFactory.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.ball2);
+        //Bitmap rotate60R = bitmapFactory.decodeResource(Constants.CURRENT_CONTEXT.getResources(), R.drawable.ball2);
 
-        idle = new Animation(new Bitmap[]{idleImg}, 1);
-        moveRight = new Animation(new Bitmap[]{idleImg, rotate30R, rotate60R}, 0.2f);
+        //idle = new Animation(new Bitmap[]{idleImg}, 1);
+        //moveRight = new Animation(new Bitmap[]{idleImg, rotate30R, rotate60R}, 0.2f);
 
-        Matrix mat = new Matrix();
-        mat.preScale(-1, 1);
-        Bitmap rotate30L = Bitmap.createBitmap(rotate30R, 0, 0, rotate30R.getWidth(), rotate30R.getHeight(), mat, false);
-        Bitmap rotate60L = Bitmap.createBitmap(rotate60R, 0, 0, rotate60R.getWidth(), rotate60R.getHeight(), mat, false);
+        //Matrix mat = new Matrix();
+        //mat.preScale(-1, 1);
+        //Bitmap rotate30L = Bitmap.createBitmap(rotate30R, 0, 0, rotate30R.getWidth(), rotate30R.getHeight(), mat, false);
+        //Bitmap rotate60L = Bitmap.createBitmap(rotate60R, 0, 0, rotate60R.getWidth(), rotate60R.getHeight(), mat, false);
 
-        moveLeft = new Animation(new Bitmap[]{idleImg, rotate30R, rotate60R}, 0.2f);
+        //moveLeft = new Animation(new Bitmap[]{idleImg, rotate30R, rotate60R}, 0.2f);
 
-        animationManager = new AnimationManager(new Animation[]{idle, moveRight, moveLeft});
+        //animationManager = new AnimationManager(new Animation[]{idle, moveRight, moveLeft});
     }
 
     @Override
     public void draw(Canvas canvas) {
-        //Paint paint = new Paint();
-        //paint.setColor(color);
-        //canvas.drawRect(rect, paint);
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawRect(rect, paint);
 
-        animationManager.onDraw(canvas, rect);
+        //animationManager.onDraw(canvas, rect);
     }
 
-    @Override
+    //@Override
     public void update() {
-        animationManager.update();
+        //animationManager.update();
     }
 
     public void update(Point point) {
-        float oldLeft = rect.left;
+        //float oldLeft = rect.left;
 
         rect.set(point.x - rect.width()/2, point.y - rect.height()/2, point.x + rect.width()/2, point.y + rect.height()/2);
 
-        int moveState = 0;
-        if(rect.left - oldLeft > 5)
-            moveState = 1;
-        else if(rect.left - oldLeft < -5)
-            moveState = 2;
+        //int moveState = 0;
+        //if(rect.left - oldLeft > 5)
+            //moveState = 1;
+        //else if(rect.left - oldLeft < -5)
+            //moveState = 2;
 
-        animationManager.playAnimation(moveState);
-        animationManager.update();
+        //animationManager.playAnimation(moveState);
+        //animationManager.update();
     }
 
     public Rect getRect(){
